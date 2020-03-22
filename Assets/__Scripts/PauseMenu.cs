663157cc1846cdf26063cd217e37a8237d544211
Signe Eide <signe.eide@live.no>
2020-03-22 16:00:32 -0400

    Made pause screen
    This menu calls on functions but they dont do much yet. But the astructure is ready.

---
+++ Assets/__Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false; //This can now be accessed elsewhere. Like turning the volume down while the game is paused ect.
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused) Resume();
+            else Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; //At 1f time will runn as normal. Here its posible to add slow motion or something
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; //Pauses the in game time!
+        gameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading menu..");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game.");
+    }
+}
