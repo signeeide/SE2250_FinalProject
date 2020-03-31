48d36edf7522eec20e73ac02d07eb37cda3b9dcc
Signe Eide <signe.eide@live.no>
2020-03-31 00:15:17 -0400

    Made an opening cutscene
    The player now starts outside the castle. It should end up inside, but since scene0 is not finished, this must be changed later..!

---
+++ Assets/__Scripts/OpeningCutscene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class OpeningCutscene : MonoBehaviour
+{
+    public float delayedStart = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Invoke start");
+        Invoke("StartGame", delayedStart);
+    }
+
+    private void StartGame()
+    {
+        Debug.Log("Trying to start game");
+        //TO DO: Change to scene0 when that scene is done
+        Main.ChangeScene("Scene1");
+    }
+}
